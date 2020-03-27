10e7df0539a12d1443b49f54d7fc5aafa5bd4c35
juliedakouré <julie.dakoure@outlook.fr>
2020-03-27 19:28:07 +0100

    to be continued sweet alert

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,15 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
+
+import { initSweetalert } from '../plugins/init_sweetalert';
+
+
+if (document.querySelector('#sweet-alert-demo')) {
+  initSweetalert('#sweet-alert-demo', {
+    title: "Merci d'avoir donné ton avis :) ",
+    buttons: false,
+    timer: 12000,
+    text: "Check the status of your booking on your dashboard",
+    icon: "success"
+  });
