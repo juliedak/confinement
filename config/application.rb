fe5d14be621b6003adf9483855cb40ec1b64a1fd
juliedakouré <julie.dakoure@outlook.fr>
2020-03-28 10:52:33 +0100

    changing name

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module ConfinementActivities
+module Confinement
   class Application < Rails::Application
     config.generators do |generate|
           generate.assets false
